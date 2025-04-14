6042b3b9a3f445ad252837443965a0e5c95f1440
arifmalabar <ridhoarif40@gmail.com>
2025-04-14 21:45:52 +0700

    form fix

--- public/js/helper/form_validation.js
+++ public/js/helper/form_validation.js
@@ -3,6 +3,13 @@ export function validateEmptyField(field) {
         throw new Error("Field harus diisi");
     }
 }
+export function validateEmptyFields(fields) {
+    fields.forEach((field) => {
+        if (field === "") {
+            throw new Error(`Field ${field} harus diisi`);
+        }
+    });
+}
 export function validateTrueFalseField(field1, field2) {
     if (field1 != field2) {
         throw new Error("Data tidak sama");
